f81bc56fd0796405e81d534cd736e2782125b149
Anton <example.gmail.com>
2021-07-16 15:25:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+console.log('arr' + ' - object');
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+// ++incr;
+// --decr;
+
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(2 + 2 * 2 !== '6');
+
+const isChecked = true,
+      isClose = false;
+
+console.log(!isChecked || isClose);
